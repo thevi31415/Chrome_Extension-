f90c301a21ab5bce63684115b1880727d3611f9d
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2022-12-09 17:45:50 +0700

    Second file updated

--- Google_extension/custom.js
+++ Google_extension/custom.js
@@ -1,7 +1,23 @@
-let htmlTemplate = 
-<div class ="facebook-color">
-  <button type="button" class ="Red">Đỏ</button>
-  <button type="button" class ="Yellow">Vàng</button>
-
-</div>;
+var timeId = "time"; 
+var dateId = "date"; 
+var days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]; 
+var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]; 
+var consoleGreeting = "Hello World! - from popup_script.js"; 
+function setTimeAndDate(timeElement,dateElement) { 
+ var date = new Date(); 
+ var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes(); 
+ var time = date.getHours() + ":" + minutes; 
+ //In "date.getMonth", 0 indicates the first month of the year 
+ //In "date.getDay", 0 represents Sunday 
+ var date = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear(); 
+ timeElement.innerHTML = time; 
+ dateElement.innerHTML = date; 
+} 
+console.log(consoleGreeting); 
+document.addEventListener("DOMContentLoaded",function(dcle) { 
+ var timeElement = document.getElementById(timeId); 
+ var dateElement = document.getElementById(dateId); 
+ setTimeAndDate(timeElement,dateElement); 
+});
+alert("Hello world!");
 
